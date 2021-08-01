5889498f4ad5514c28ff2615afff9541975f4dad
gresier <87758321+gresier@users.noreply.github.com>
2021-08-01 13:13:18 +0200

    Update autotap.lua

--- roblox-scripts/autotap.lua
+++ roblox-scripts/autotap.lua
@@ -1,4 +1,5 @@
--- in beta
+-- ver 1.0.0.0
+-- autotap
 local games=({
   Tapping_Mania=true
   Tapping_Simulator=false
@@ -9,7 +10,7 @@ if games.Tapping_Mania==true then
   if game.PlaceId==473274832 then
     while true do
       if games.autotap==true then
-        --
+        game:GetService("ReplicatedStorage").RemoteEvents.Click:FireServer()
       end
     end
   end
@@ -17,7 +18,7 @@ elseif games.Tapping_Simulator==true then
   if game.PlaceId==473274832 then
     while true do
       if games.autotap==true then
-        --
+        game:GetService("ReplicatedStorage").PlayerClick:FireServer()
       end
     end
   end
@@ -25,7 +26,7 @@ elseif games.Tapping_Gods==true then
   if game.PlaceId==473274832 then
     while true do
       if games.autotap==true then
-        --
+        game:GetService("ReplicatedStorage").Events.Tap:FireServer()
       end
     end
   end
