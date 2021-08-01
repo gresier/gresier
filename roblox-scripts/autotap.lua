1a9000a6f6e13807d222f5fbb5d0027e1c15c677
gresier <87758321+gresier@users.noreply.github.com>
2021-08-01 13:09:09 +0200

    Create autotap.lua

---
+++ roblox-scripts/autotap.lua
@@ -0,0 +1,32 @@
+-- in beta
+local games=({
+  Tapping_Mania=true
+  Tapping_Simulator=false
+  Tapping_Gods=false
+  autotap=true
+})
+if games.Tapping_Mania==true then
+  if game.PlaceId==473274832 then
+    while true do
+      if games.autotap==true then
+        --
+      end
+    end
+  end
+elseif games.Tapping_Simulator==true then
+  if game.PlaceId==473274832 then
+    while true do
+      if games.autotap==true then
+        --
+      end
+    end
+  end
+elseif games.Tapping_Gods==true then
+  if game.PlaceId==473274832 then
+    while true do
+      if games.autotap==true then
+        --
+      end
+    end
+  end
+end
